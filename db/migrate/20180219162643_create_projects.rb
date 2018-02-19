b100de918599e82b45a2b09dd2c90dfd734bdede
Theo Nazon <theonazon@gmail.com>
2018-02-19 17:32:40 +0100

    Ajout du modele de base pour Project

---
+++ db/migrate/20180219162643_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :content
+      t.string :department
+      t.string :city
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
