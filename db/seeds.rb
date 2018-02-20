4de5c174c894208c2307abc6c345ad1c3d1d963a
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-20 14:17:22 +0100

    Add models validations and links plus creation of the seed file with categories, projects and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 require "open-uri"
 require "nokogiri"
 
+Category.destroy_all
+User.destroy_all
+Projects.destroy_all
+
 puts "Creating categories..."
 
 html_content = open('https://fr.wikipedia.org/wiki/M%C3%A9tiers_du_b%C3%A2timent#Fumisterie').read
@@ -13,4 +17,29 @@ end
 Category.create!(name: "Isolation")
 Category.create!(name: "Plomberie")
 Category.create!(name: "Electricité")
-Category.create!(name: "Isolation")
+
+
+puts "Creating users..."
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    city: Faker::Address.city,
+    address: Faker::Address.street_address,
+    zip: (01000..99999).to_a.sample
+  )
+end
+
+puts "Creating projects..."
+10.times do
+  Project.create!(
+    title: Faker::Hipster.sentence,
+    content: Faker::Hipster.paragraph,
+    city: Faker::Address.city,
+    department: (0..99).to_a.sample,
+    category_id: ((Category.first.id)..Category.count).to_a.sample,
+    user_id: ((User.first.id)..(User.last.id)).to_a.sample
+  )
+end
