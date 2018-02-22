e5a0cbcc0f57a99923716980561bf2a83431e654
alexandrebk <alexandre.b2506@gmail.com>
2018-02-22 17:32:23 +0100

    outil basique de search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @projects = Project.all
+    if params[:query].present?
+      sql_query = " \
+        projects.title @@ :query \
+        OR projects.content @@ :query \
+        OR categories.name @@ :query \
+      "
+      @projects = Project.joins(:category).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @projects = Project.all
+    end
   end
 end
