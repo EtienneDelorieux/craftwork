9ac10fc0863b43ab88d87240178c90d4386eb898
louis delon <louleman34@gmail.com>
2018-02-22 13:35:02 +0100

    add & install pundit

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,9 @@
 module ProjectsHelper
   def project_image_path_for(project)
-    cl_image_path project.photo
+    if project.photo.present?
+      cl_image_path project.photo
+    else
+      'http://placehold.it/400x400'
+    end
   end
 end
